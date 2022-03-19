fe06401b7acfcf138bcb17fa64701d6980a35739
laziza@mail.ru <laziza00>
2022-03-19 19:35:26 +0500

    initial commit

---
+++ integer.js
@@ -0,0 +1,27 @@
+// let arr =[1, 2, 3, 4, 5, 6,7, 8];
+
+
+
+// let = sum = arr.map((el, i, arr) => el*2);
+// console.log(sum);
+
+// sum =[]
+// for (let i=0; i<arr.length; i++){
+//     console.log(sum.push(arr[i]));
+// }
+
+let arr = [20, 10, 8, 23, 33]
+
+// let sum = arr.filter(el => el>=18);
+// console.log(sum);
+b =[]
+
+function filter1 (a, b){
+
+    for(let i=0; i<a.lenght; i++){
+        if(a[i]>=b){
+            b.push(a[i])
+        }
+    }
+}
+// console.log(, 18));
